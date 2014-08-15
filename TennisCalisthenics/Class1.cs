11bb9b532ac74171cc8e29a4a42d1b466dde2c91
Dan Kalotay <daniel.kalotay@7digital.com>
2014-08-15 16:47:49 +0100

    initial commit
    this i not good practice

---
+++ TennisCalisthenics/Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TennisCalisthenics
+{
+	public class Game
+	{
+		private readonly Scores _scores;
+
+		public Game(Action<IPlayer> winnerListener)
+		{
+			_scores = new Scores(winnerListener);
+		}
+
+		public void Score(IPlayer player)
+		{
+			_scores.AddPointFor(player);
+		}
+
+	}
+
+	public class Scores
+	{
+		private readonly Action<IPlayer> _winnerListener;
+		private readonly List<IPlayer> _scores = new List<IPlayer>();
+
+		public Scores(Action<IPlayer> winnerListener)
+		{
+			_winnerListener = winnerListener;
+		}
+
+		public void AddPointFor(IPlayer player)
+		{
+			_scores.Add(player);
+			if (_scores.Count(x => x == player) == 4)
+			{
+				_winnerListener(player);
+			}
+
+		}
+	}
+
+	public interface IPlayer
+	{
+	}
+
+	public class Server : IPlayer
+	{
+		
+	}
+
+	public class Receiver : IPlayer
+	{
+		
+	}
+}
+
+namespace TennisCalisthenics.Tests.Unit
+{
+	[TestFixture]
+	public class GameTests
+	{
+		private IPlayer _winner;
+
+		[SetUp]
+		public void ResetWinner()
+		{
+			_winner = null;
+		}
+
+		private void OnWinner(IPlayer winningPlayer)
+		{
+			_winner = winningPlayer;
+		}
+		
+		[Test]
+		[TestCase(typeof(Server))]
+		[TestCase(typeof(Receiver))]
+		public void Game_is_won_with_at_least_four_points_scored(Type playerType)
+		{
+			var player = playerType == typeof(Server) ? (IPlayer)new Server() : new Receiver();
+			var game = new Game(OnWinner);
+
+			game.Score(player);
+			game.Score(player);
+			game.Score(player);
+			game.Score(player);
+
+			Assert.That(_winner, Is.EqualTo(player));
+		}
+
+		[Test]
+		public void Two_points_extra_are_needed_to_win_the_game()
+		{
+			var game = new Game(OnWinner);
+			var server = new Server();
+			var receiver = new Receiver();
+
+			game.Score(server);
+			game.Score(server);
+			game.Score(server);
+			game.Score(server);
+			game.Score(receiver);
+			game.Score(receiver);
+
+			Assert.That(_winner, Is.EqualTo(server));
+		}
+
+	}
+}
---
+++ TennisCalisthenics/FsmMatch.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TennisCalisthenics
+{
+	public interface IGame
+	{
+		void ReceiverScore();
+
+		void ServerScore();
+	}
+
+	public class InitialGame: IGame
+	{
+		private readonly IWinnerListener _listener;
+
+		public InitialGame(IWinnerListener listener)
+		{
+			_listener = listener;
+		}
+
+		public void ReceiverScore()
+		{
+		}
+
+		public void ServerScore()
+		{
+		}
+	}
+
+	public interface IPoint
+	{
+		IPoint Increment();
+	}
+
+	public class Zero: IPoint
+	{
+		public IPoint Increment()
+		{
+			return new Fifteen();
+		}
+	}
+
+	public class Fifteen : IPoint
+	{
+		public IPoint Increment()
+		{
+			return new Thirty();
+		}
+	}
+
+	public class Thirty : IPoint
+	{
+		public IPoint Increment()
+		{
+			return new Fourty();
+		}
+	}
+
+	public class Fourty : IPoint
+	{
+		public IPoint Increment()
+		{
+			throw new Exception("not sure what to do");
+		}
+	}
+
+	public interface IWinnerListener
+	{
+		void ServerWon();
+		void ReceiverWon();
+	}
+
+	[TestFixture]
+	public class MatchTest: IWinnerListener
+	{
+		private enum Player
+		{
+			Server,
+			Receiver
+		}
+
+		private Player? _winner;
+		private IGame _game;
+
+		[SetUp]
+		public void ResetWinner()
+		{
+			_winner = null;
+			_game = new InitialGame(this);
+		}
+
+		[Test]
+		public void Server_scoring_four_times_means_server_wins()
+		{
+			_game.ServerScore();
+			_game.ServerScore();
+			_game.ServerScore();
+			_game.ServerScore();
+
+			Assert.That(_winner, Is.EqualTo(Player.Server));
+		}
+
+		[Test]
+		public void Receiver_scoring_four_times_means_server_wins()
+		{
+			_game.ReceiverScore();
+			_game.ReceiverScore();
+			_game.ReceiverScore();
+			_game.ReceiverScore();
+
+			Assert.That(_winner, Is.EqualTo(Player.Receiver));
+		}
+
+		[Test]
+		public void Incomplete_games_have_no_winner()
+		{
+			_game.ReceiverScore();
+			_game.ServerScore();
+			_game.ServerScore();
+			_game.ReceiverScore();
+
+			Assert.That(_winner, Is.Null);
+		}
+
+		public void ServerWon()
+		{
+			_winner = Player.Server;
+		}
+
+		public void ReceiverWon()
+		{
+			_winner = Player.Receiver;
+		}
+	}
+}
